b1171c3d15515600de63ad73197925f4c90480b2
kbdavid15 <kyle.david@redline-electronics.com>
2015-07-21 21:32:16 -0400

    Updated plot function to support magnetometer

--- plot9dof.m
+++ plot9dof.m
@@ -17,16 +17,29 @@ legend('Accel X', 'Accel Y', 'Accel Z')
 title('Acceleration Output');
 ylabel('Accel - g');
 
-if (s(2) >=6)
+if (s(2) >=6)    
     gyro_x = D.data(:, 4);
     gyro_y = D.data(:, 5);
     gyro_z = D.data(:, 6);
     subplot(3,1,2)
+    hold all
     plot(gyro_x)
     plot(gyro_y)
     plot(gyro_z)
     title('Gyroscope Output');
     ylabel('Degrees Per Second');
+    if (s(2) >=9)
+        mag_x = D.data(:, 7);
+        mag_y = D.data(:, 8);
+        mag_z = D.data(:, 9);
+        subplot(3,1,3)
+        hold all
+        plot(mag_x)
+        plot(mag_y)
+        plot(mag_z)
+        title('Magnetometer Output');
+        ylabel('Gauss');
+    end
 end
 
 
